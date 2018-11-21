d7e7c2ddb4d92e855bdc210a45d8d85763491c3a
feridHelali <helaliferid@gmail.com>
2018-11-21 13:08:48 +0100

    add multer

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ mongoose.connect(config.remote);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pizzaRouter = require ('./routes/pizza');
+var pictureRouter = require('./routes/picture');
 
 
 var app = express();
@@ -22,10 +23,12 @@ app.use(logger('dev'));
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pizza',pizzaRouter);
+app.use('/uploads',pictureRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/picture.js
@@ -0,0 +1,50 @@
+var express =require('express');
+var multer=require('multer');
+var Q =require('q');
+var router=express.Router();
+var Pizza=require('../schemas/pizza-schemas');
+
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function(req, file, cb) {
+        cb(null, './uploads/pizza-pictures');
+    },
+    filename: function(req, file, cb) {
+        var pizzaId = req.body.pizzaId;
+        var path = file.fieldname + '-' + pizzaId + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1];
+        cb(null, path);
+
+        updatePicture(pizzaId, path)
+            .then(function() {
+                res.sendStatus(200);
+            })
+            .catch(function(err) {
+                res.status(400).send(err);
+            });
+    }
+});
+
+var upload = multer({ //multer settings
+    storage: storage
+}).single('picture');
+
+function updatePicture(id, path) {
+    var deferred = Q.defer();
+    Pizza.findByIdAndUpdate({ _id: id }, { $set: { picture: path } },
+        function(err, doc) {
+            if (err) deferred.reject(err.name + ': ' + err.message);
+            deferred.resolve();
+        });
+    return deferred.promise;
+}
+
+router.post('/picture', upload, function(req, res, next) {
+    upload(req, res, function(err) {
+        if (err) {
+            res.json({ error_code: 1, err_desc: err });
+            return;
+        }
+        res.json({ error_code: 0, err_desc: 'Avatar successfully uploded' });
+    });
+});
+
+module.exports = router;
