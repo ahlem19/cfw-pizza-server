6a04e9eaa80f40437b473d2a9681dfe04e4c19f8
helaliFerid <helaliferid@gmai.com>
2018-11-26 10:37:07 +0100

    add createAt to pizza schema

--- schemas/pizza-schemas.js
+++ schemas/pizza-schemas.js
@@ -1,23 +1,24 @@
-var mongoose=require('mongoose');
-var schema=mongoose.Schema;
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
 
-const pizzaSchema=new schema({
-	label:{
-		type:String,
-		required:true
-	},
-	ingredient:[{
-		type:String,
-		required:true
-	}],
-	price:{
-		type:Number
-	},
-	picture:{
-		type:String,
-        required:true,
-        default:'http://la-cucina.fr/wp-content/uploads/2014/07/gal1.jpg'
-	}
-});
+const pizzaSchema = new schema({
+    label: {
+        type: String,
+        required: true
+    },
+    ingredients: [{
+        type: String,
+        required: true
+    }],
+    price: {
+        type: Number
+    },
+    picture: {
+        type: String,
+        required: false,
+        default: 'http://la-cucina.fr/wp-content/uploads/2014/07/gal1.jpg'
+    },
 
-module.exports=mongoose.model('Pizza',pizzaSchema);
+}, { timestamps: { createdAt: 'created_at' } });
+
+module.exports = mongoose.model('Pizza', pizzaSchema);
